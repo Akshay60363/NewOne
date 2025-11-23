702e75523d2d37a8d388045bd609721db9b67cc5
Akshay Raaj CS <akshay.raaj124@gmail.com>
2025-11-23 17:32:22 +0530

    iniy

---
+++ src/Hogo.java
@@ -0,0 +1,8 @@
+
+public class Hogo {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, Hogo!");
+	}
+
+}
---
+++ src/Mane.java
@@ -0,0 +1,8 @@
+
+public class Mane {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, World!");
+	}
+
+}
