9f2314892a7f8df31bc22164ca90d4b536d95002
Akshay Raaj CS <akshay.raaj124@gmail.com>
2025-11-23 17:47:37 +0530

    hfjd

--- src/Hogo.java
+++ src/Hogo.java
@@ -3,6 +3,9 @@ public class Hogo {
 
 	public static void main(String[] args) {
 		System.out.println("Hello, Hogo!");
+		for (int i = 0; i < 5; i++) {
+			System.out.println("Count:" + i);
+		}
 	}
 
 }
